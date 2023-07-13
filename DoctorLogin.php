bbd36b5d8a5898013cd6086025c77493a9832a8a
Victor Kipngetich <victor.kipngetich@strathmore.edu>
2023-07-13 13:27:42 +0300

    Doctor's page functionalities

---
+++ DoctorLogin.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) {
+
+    header('Location: doctorPage.php');
+    exit();
+}
+$SSN = $_POST['doctorssn'];
+$password = $_POST['password'];
+
+$error = '';
+
+
+
+if (empty($SSN)) {
+    $error .= '<p class="error">Please enter your Number.</p>';
+}
+
+if (empty($password)) {
+    $error .= '<p class="error">Please enter password.</p>';
+}
+if (empty($error)) {
+
+    if ($query = $conn->prepare("SELECT FirstName,DoctorSSN,password FROM doctor WHERE DoctorSSN = ?")) {
+
+        $query->bind_param('i', $SSN);
+        $query->execute();
+        $query->bind_result($name, $doctorSSN, $pass);
+        $row = $query->fetch();
+
+        if ($row) {
+
+            if (password_verify($password, $pass)) {
+
+                $_SESSION["SSN"] = $doctorSSN;
+                $_SESSION["name"] = $name;
+                $_SESSION['loggedIn'] = true;
+
+
+
+
+
+                header("Location: doctorPage.php ");
+                exit;
+            } else {
+                echo '<p class="error">The password is not valid.</p>';
+            }
+        } else {
+            echo '<p class="error">No User exist with that number.</p>';
+        }
+    }
+}

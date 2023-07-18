26a2834911b1658b4512c696c013db678e166084
Victor Kipngetich <victor.kipngetich@strathmore.edu>
2023-07-18 10:26:57 +0300

    add pharmacist drug dispensing feature

--- pharmacistpage.php
+++ pharmacistpage.php
@@ -31,7 +31,7 @@ if ($_SESSION['loggedIn']) : ?>
         <form method="post" action="">
             <input type="submit" name="select" value="View Submissions">
         </form>
-        <?
+        <?php
         if (isset($_POST['select'])) {
             $today = Date("Y-m-d");
             $view = "SELECT  PrescriptionID,doctor.FirstName AS doctorName, patient.FirstName AS patientName FROM prescription_drug JOIN prescription ON 
@@ -39,7 +39,7 @@ if ($_SESSION['loggedIn']) : ?>
              JOIN patient ON consutltation.PatientSSN = patient.PatentSSN Join doctor ON patient.PrimaryDoctor = doctor.doctorSSN WHERE prescription.PrescriptionDate = ?";
             $query = $conn->prepare($view);
             if (!$query) {
-                echo "An error occured" . $query . $conn->error;
+                echo "An error occured" . $conn->error;
             } else {
                 $query->bind_param('s', $today);
                 $query->execute();
@@ -65,7 +65,7 @@ if ($_SESSION['loggedIn']) : ?>
             }
         }
         ?>
-        <h4>View detailed prescription deatails</h4>
+        <h4>View detailed prescription </h4>
         <form method="post" action="">
             <P>
                 <label for="PresciptionId">PrescriptionId:</label>
@@ -96,4 +96,65 @@ if ($_SESSION['loggedIn']) : ?>
         }
         ?>
         <h2>Dispense drugs</h2>
+        <p>Enter a presciption id dispense drugs to a patient</p>
+        <form action="" method="post">
+            <p> <label for="PrescriptionId">ID</label>
+                <input type="Number" name="prescriptionid" id="PrescriptionId">
+            </p>
+            <input type="submit" name="dispense" value="Enter">
+        </form>
+        <?php
+        if (isset($_POST['dispense']) && isset($_POST['prescriptionId'])) {
+            require_once('connect.php');
+            $id = $_POST['prescriptionId'];
+
+            $query = "SELECT pd.DrugNumber, d.TradeName, pd.Dosage, pd.Duration FROM prescription_drug pd JOIN drug d ON pd.DrugNumber = d.DrugID  WHERE pd.PrescriptionID = ?";
+            $stmt = mysqli_prepare($conn, $query);
+            mysqli_stmt_bind_param($stmt, "i", $id);
+            mysqli_stmt_execute($stmt);
+            $results = mysqli_stmt_get_result($stmt);
+
+            if ($results && mysqli_num_rows($results) > 0) {
+                echo "<table>";
+                echo "<tr><th>Drug</th><th>Dosage</th><th>Duration</th></tr>";
+                while ($row = mysqli_fetch_assoc($results)) {
+                    echo "<tr>";
+                    echo "<td>" . $row['TradeName'] . "</td>";
+                    echo "<td>" . $row['Dosage'] . "</td>";
+                    echo "<td>" . $row['Duration'] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+                echo "<p>The above drugs will be dispensed</p>";
+                echo "<form action='' method='post'>";
+                echo "<input type='submit' name='dispensed' value='Dispense'>";
+                echo "</form>";
+            } else {
+                echo "<p>No prescription data found.</p>";
+            }
+        }
+
+
+        if (isset($_POST['dispensed'])) {
+
+            $query = "SELECT DrugNumber FROM prescription_drug WHERE PrescriptionID = $prescriptionId";
+            $result = mysqli_query($conn, $query);
+            while ($row = mysqli_fetch_assoc($result)) {
+                $drugNumber = $row['DrugNumber'];
+                $queryDrug = "SELECT DrugID, Cost FROM drugs WHERE DrugNumber = $drugNumber";
+                $resultDrug = mysqli_query($conn, $queryDrug);
+                $rowDrug = mysqli_fetch_assoc($resultDrug);
+                $drugId = $rowDrug['DrugID'];
+                $cost = $rowDrug['Cost'];
+                $updateQuery = "UPDATE prescription_drug SET DrugID = $drugId, Cost = $cost WHERE PrescriptionID = $prescriptionId AND DrugNumber = $drugNumber";
+                mysqli_query($conn, $updateQuery);
+            }
+            if (mysqli_affected_rows($conn) > 0) {
+                echo "Prescription drug costs updated successfully for PrescriptionID $prescriptionId.";
+            } else {
+                echo "No prescription drug costs were updated for PrescriptionID $prescriptionId.";
+            }
+        }
+
+        ?>
         <p>Click<a href="managedrugs.php"=>here</a> to manage the drug records</p>

538728ddfd0afd7445ec4feba126e737946026f6
Victor Kipngetich <victor.kipngetich@strathmore.edu>
2023-07-18 13:55:26 +0300

    fix minor errors with database and file names

--- paregister.php
+++ paregister.php
@@ -1,35 +1,33 @@
 <?php
-require_once("connection.php");
+require_once("connect.php");
 
 
 // Add patient
-if(isset($_POST['register'])) {
+if (isset($_POST['register'])) {
 
 
-$PatientSSN = $_POST['PatientSSN'];
-$FirstName = $_POST['FirstName'];
-$SecondName = $_POST['SecondName'];
-$Address = $_POST['Address'];
-$Age = $_POST['Age'];
-$Height = $_POST['Height'];
-$Weight = $_POST['Weight'];
-$Allergies = $_POST['Allergies'];
-$PrimaryDoctor = $_POST['PrimaryDoctor'];
-$Password = $_POST['Password'];
+    $PatientSSN = $_POST['PatientSSN'];
+    $FirstName = $_POST['FirstName'];
+    $SecondName = $_POST['SecondName'];
+    $Address = $_POST['Address'];
+    $Age = $_POST['Age'];
+    $Height = $_POST['Height'];
+    $Weight = $_POST['Weight'];
+    $Allergies = $_POST['Allergies'];
+    $PrimaryDoctor = $_POST['PrimaryDoctor'];
+    $Password = $_POST['Password'];
 
-print_r($_POST);
+    print_r($_POST);
 
-$query = "INSERT INTO Patients VALUES ('$PatientSSN', '$FirstName','$SecondName','$Address','$Age','$Height','$Weight','$Allergies','$PrimaryDoctor','$Password')";
+    $query = "INSERT INTO Patient VALUES ('$PatientSSN', '$FirstName','$SecondName','$Address','$Age','$Height','$Weight','$Allergies','$PrimaryDoctor','$Password')";
 
 
     if (mysqli_query($conn, $query)) {
-        header("location: patlogin.html");
+        header("location: patlogin.php");
     } else {
         echo "Error: " . $query . "<br>" . mysqli_error($conn);
     }
-
 }
 
 
 $conn->close();
-?>
--- patlogin.php
+++ patlogin.php
@@ -1,15 +1,14 @@
-
 <?php
 
-require_once("connection.php");
+require_once("connect.php");
 
-if(isset($_POST['login'])) {
+if (isset($_POST['login'])) {
     $PatientSSN = $_POST['PatientSSN'];
     $Password = $_POST['Password'];
 
-   
+
     // Query the database to check if the user exists
-    $query = "SELECT * FROM Patients WHERE PatientSSN='$PatientSSN' AND Password='$Password'";
+    $query = "SELECT * FROM Patient WHERE PatientSSN='$PatientSSN' AND Password='$Password'";
     $result = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($result) == 1) {
@@ -30,23 +29,22 @@ if(isset($_POST['login'])) {
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet"  href="patlogin.css"/>
-        <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />        
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="patlogin.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Patient Login</title>
-        
+    <title>Patient Login</title>
 
-        <link rel="preconnect" href="http://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans: ital, wght@0,100; 0,30 0;0,400;0,500; 0,600; 0,700; 1, 100; 1, 200; 1,300; 1,400; 1,500; 1,600;1,700&family=Montserrat: wght@700; 800; 900&display=swap" 
-         rel="stylesheet">
 
-        
-    </head>
-    
-    <body>
+    <link rel="preconnect" href="http://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans: ital, wght@0,100; 0,30 0;0,400;0,500; 0,600; 0,700; 1, 100; 1, 200; 1,300; 1,400; 1,500; 1,600;1,700&family=Montserrat: wght@700; 800; 900&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
 
     <div class="hero">
         <nav>
@@ -62,7 +60,7 @@ if(isset($_POST['login'])) {
     </div>
 
 
-    
+
 
 
     <div class="login-box">
@@ -70,29 +68,29 @@ if(isset($_POST['login'])) {
 
         <form action="http://localhost/phpcode/WebAppProj/patlogin.php" method="post">
 
-        <div class="textbox">
-        <i class="fa-solid fa-user"></i>
-        <input type="number" name="PatientSSN" placeholder="Patient SSN">
-        <br>
-        </div>
+            <div class="textbox">
+                <i class="fa-solid fa-user"></i>
+                <input type="number" name="PatientSSN" placeholder="Patient SSN">
+                <br>
+            </div>
 
-        <div class="textbox">
-        <i class="fa-solid fa-lock"></i>
-          <input type="password" name="Password" placeholder="Password">
-          <br><br>
-      
-        </div>
-          
-          <input class= "btn" type="submit" name="login" value="Login">
-        </form> 
+            <div class="textbox">
+                <i class="fa-solid fa-lock"></i>
+                <input type="password" name="Password" placeholder="Password">
+                <br><br>
+
+            </div>
+
+            <input class="btn" type="submit" name="login" value="Login">
+        </form>
 
 
 
-        <p><a href = "http://localhost/phpcode/WebAppProj/paregister.html">Don't have an account? Register</a></p> 
+        <p><a href="http://localhost/phpcode/WebAppProj/paregister.html">Don't have an account? Register</a></p>
 
     </div>
 
-    
-    </body>
-</html>
 
+</body>
+
+</html>

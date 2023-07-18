e09303ebf4007c9563ce7da8aef20fcd5a20fdaf
kn_sid <58786874+Sidney-Nduti@users.noreply.github.com>
2023-07-18 13:38:59 +0300

    The mainpage and the css.
    This only includes the navigation bar

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+if($_SESSION['PatientSSN']) {
+echo "Welcome user " . $_SESSION['PatientSSN'];
+}else{
+header("location: patlogin.php");
+}
+?>
+<a href="logout.php">LogOut</a>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="indexstyles.css">
+
+    <link rel="preconnect" href="http://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans: ital, wght@0,100; 0,30 0;0,400;0,500; 0,600; 0,700; 1, 100; 1, 200; 1,300; 1,400; 1,500; 1,600;1,700&family=Montserrat: wght@700; 800; 900&display=swap" 
+    rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="hero">
+        <nav>
+            <h2 class="logo">Drug<span>Tool</span></h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <button type="button" onclick="window.location.href = 'patlogin.php';">Login</button>
+        </nav>
+    </div>
+
+
+
+</body>
+</html>

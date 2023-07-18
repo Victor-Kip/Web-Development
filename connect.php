b12ca181c9f17f1e6f94cee4178c1bf6b68ee972
Victor Kipngetich <victor.kipngetich@strathmore.edu>
2023-07-18 20:08:30 +0300

    add file that connects to the database

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password  = "MyOscVic2@";
+$database = "HospitalDB";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	echo "<br>";
+}
+echo "Connection successful";
+echo "<br>";
